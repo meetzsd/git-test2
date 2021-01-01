c9a1fe66c9b32b222faf6dd92775d3e65cc12d61
meetzsd <meetzsd@gmail.com>
2021-01-01 10:57:38 +0800

    test22

--- src/Myapplication.java
+++ src/Myapplication.java
@@ -2,5 +2,6 @@ public class Myapplication {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("123");
+        System.out.println("123");
     }
 }
